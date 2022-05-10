f10c301aac0bbd2eea1dd251d8a824c0f20b304e
Luca Viggiani <luca.viggiani@grammelot.eu>
2022-05-10 21:07:35 +0200

    dev

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,6 @@
 import { Widgets } from "/vee/index.js";
 
 async function main(){
-    const response = await fetch("/api/obligationes", { 
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-
-    console.log(await response.json());
-
-    console.log(Widgets);
 }
 
 window.addEventListener("load", main);
